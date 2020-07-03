907754a3d884b8ada1b6f2b1b802d4e2d82671f2
Rudolf <rudolfcickodev@gmail.com>
2020-07-03 16:08:18 +0200

    Improving

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Editor from 'react-simple-code-editor';
 import dedent from 'dedent';
 import { highlight, languages } from 'prismjs/components/prism-core';
 import { SocialIcon } from 'react-social-icons';
-import { FaTree, FaHeart, FaCopyright, FaEdit, FaStar, FaCoffee } from 'react-icons/fa';
+import { FaTree, FaHeart, FaCopyright, FaEdit, FaStar, FaCoffee, FaDownload } from 'react-icons/fa';
 import JSONTree from 'react-json-tree'
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
@@ -59,6 +59,7 @@ class App extends React.Component {
           result = result.map(item => {
             if (!item[key]) {
               warningMessage = `'${key}' key inside ${JSON.stringify(item)} doesn't exist`;
+              return item;
             }
             return item[key]
           });
@@ -84,7 +85,7 @@ class App extends React.Component {
 
 
     this.setState({ 
-      result: JSON.stringify(result, null, ' '), 
+      result: JSON.stringify(result, null, '\t'), 
       parsedInput: code, 
       warningMessage,
       errorMessage,
@@ -116,17 +117,24 @@ class App extends React.Component {
     }
   }
 
-  renderInviteMeACoffee = () => <div className="invite-coffee"><a target="_blank" href="https://paypal.me/rudolfcicko">Invite me a coffee :) </a></div> 
+  renderInviteMeACoffee = () => 
+    <a href="https://www.buymeacoffee.com/rudolfcicko" target="_blank">
+      <img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" 
+        style={{ height: 33, width: 139}}/>
+    </a>
 
   render() {
     return (
         <main className="container">
-          {this.renderInviteMeACoffee()}
           <div className="container__content">
             <h1>JSON Mapper</h1>
             <p>Easy way to extract the data you want from your json object.</p>
-            <input value={this.state.input} onChange={this.onInputChange}/>
-            <button onClick={this.computeResult}> Compute </button>
+            <input value={this.state.input} onChange={this.onInputChange} onKeyDown={(e) => {
+              if (e.keyCode === 13) {
+                this.computeResult();
+              }
+            }}/>
+            <button onClick={this.computeResult}> Generate result </button>
             {this.state.errorMessage &&
               <div className="error">
                 {this.state.errorMessage}
@@ -139,10 +147,12 @@ class App extends React.Component {
             }
             <div className="container_wrapper">
               <div className="container_editor_area">
+                {/*
                 <div className="container_editor_tools">
-                  <FaEdit title="edit view" onClick={() => this.setState({ viewMode: 'edit'})}/>
-                  <FaTree title="tree view" onClick={() => this.setState({ viewMode: 'tree'})}/>
+                  <FaEdit style={{ color: '#9e9e9e' }} title="edit view" onClick={() => this.setState({ viewMode: 'edit'})}/>
+                  <FaTree style={{ color: '#9e9e9e' }} title="tree view" onClick={() => this.setState({ viewMode: 'tree'})}/>
                 </div>
+                */}
                 {this.renderInputByMode()}
               </div>
               <div className="container_editor_area">
@@ -153,17 +163,35 @@ class App extends React.Component {
                     padding={20}
                     className="container__editor"
                 />
+                {this.state.result && <div className="container_result_tools">
+                  <FaDownload title="Download result in json file" onClick={() => {
+                    alert('Coming soon...')
+                  }}/>
+                </div>}
               </div>
             </div>
           </div>
           <div className="container__footer">
-            <p>
-              Let me know if you like this tool to continue working on it. Collaborate or <a href="https://github.com/Cicko/json-mapper" target="_blank">give me a <FaStar style={{ color: '#F4B400' }}/> :) </a>
-            </p>
+            <div className="footer_first-phrase">
+              <p>
+                Let me know if you like this tool to continue working on it.&nbsp; 
+                <a href="https://github.com/cicko/json-mapper/pulls" target="_blank">
+                  Collaborate
+                </a>  
+                &nbsp;or&nbsp;
+                <a href="https://github.com/Cicko/json-mapper" target="_blank">
+                  give me a ⭐ :) 
+                </a>
+              </p>
+            </div>
             <footer>
-            <p>
-              Created with   &nbsp;<FaHeart style={{ color: '#DB4437'}} />   &nbsp; by &nbsp;<a target="_blank" href="https://github.com/Cicko">Rudolf Cicko</a> &nbsp; © &nbsp; 2020
-            </p>
+            <div className="footer__copyright">
+            <a target="_blank" href="https://github.com/Cicko">
+              Created with&nbsp;
+              <FaHeart style={{ color: '#DB4437'}} />
+              &nbsp;by&nbsp;Rudolf Cicko&nbsp;©&nbsp;2020</a>
+            </div>
+            {this.renderInviteMeACoffee()}
             </footer>
           </div>
         </main>
